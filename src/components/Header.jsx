27342ee8a38a259c0bd7edeaccc44f3f6e4153ac
bossTodev <bossongsaza@gmail.com>
2024-04-29 22:55:49 +0700

    add first

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <img src='/images/logo.png'></img>
+        </div>
+        <div>
+            logo
+        </div>
+        <div>
+            search
+        </div>
+        <div>
+            profile
+        </div>
+    </div>
+  )
+}
+
+export default Header
