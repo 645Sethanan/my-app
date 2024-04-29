36426b3a90daf7a1c1d02e1cf6cd22ac82f676a3
oMoZERAo <armkungza3691@gmail.com>
2024-04-29 23:10:42 +0700

    add third

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <img src='/images/logo.png'></img>
         </div>
         <div>
-            logo sdfasdfwas
+            logo sdfasdfwasdsafsafhskafs
         </div>
         <div>
             search
